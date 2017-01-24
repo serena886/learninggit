bbc9dbaec5064060138658565086893370fac597
connolly.liu <connolly.liu@vipshop.com>
2017-01-24 15:21:48 +0800

    changes twice

--- BusAssemblePayServiceImpl.java
+++ BusAssemblePayServiceImpl.java
@@ -8,7 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
+first change
 add sth
 import com.vip.xfd.trade.adapter.LicaiPayAdapter;
 import com.vip.xfd.trade.common.manager.BaseManager;
